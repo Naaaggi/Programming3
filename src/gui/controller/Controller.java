2431b297725e7a7c003a942620c2248481508ae7
Naaaggi <naguigh23@gmail.com>
2022-07-30 17:25:37 +0200

    fixed gui

--- src/gui/controller/Controller.java
+++ src/gui/controller/Controller.java
@@ -1,40 +1,44 @@
 package gui.controller;
 
-import gui.GUI;
 import io.*;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
 import javafx.scene.control.ListView;
+import javafx.scene.control.TableView;
 import javafx.scene.control.TextField;
 import javafx.scene.control.Alert;
 import javafx.scene.input.MouseEvent;
 
-import java.awt.*;
+
+import java.util.ArrayList;
 import java.util.ArrayList;
 import java.util.Random;
-import java.io.ObjectInputStream;
-import java.io.ObjectOutputStream;
-import java.io.Serializable;
-import java.nio.file.Files;
-import java.nio.file.Paths;
+
 
 public class Controller {
     Alert errorAlert = new Alert(Alert.AlertType.ERROR);
     @FXML
-    private ListView<String> MediaFileList;
+    private TableView<String> MediaFileList;
     @FXML
     private ListView<String> UploaderList;
     @FXML
     private TextField MediaFileName;
-    @FXML
-    private TextField Producer;
 
     @FXML
     void createMedia(MouseEvent event) {
-        MediaFileList.getItems().add(MediaFileName.getText());
-        UploaderList.getItems().add(Producer.getText());
-        System.out.println("Media added: " + MediaFileName.getText() + " by " + Producer.getText());
+        String text = MediaFileName.getText();
+        String[] parsed = text.split(" ");
+        String producer = parsed[1];
+        MediaFileList.getItems().add(text);
+        if (UploaderList.getItems().contains(producer)) {
+            errorAlert.setTitle("Error");
+            errorAlert.setHeaderText("Uploader already exists");
+            errorAlert.showAndWait();
+        } else {
+            UploaderList.getItems().add(producer);
+        }
+        System.out.println("Media added: " + MediaFileName.getText() + " by " + parsed[1]);
     }
 
     @FXML
@@ -67,15 +71,25 @@ public class Controller {
     @FXML
     void saveData(MouseEvent event) {
         SaveData data = new SaveData();
-        ArrayList<String> writeList = new ArrayList<String>();
+        SaveData data1 = new SaveData();
+        ArrayList<String> writeMediaList = new ArrayList<String>();
+        ArrayList<String> writeUploaderList = new ArrayList<String>();
 
 // Loop through the observable list and load the string array
         for (int i =0; i<MediaFileList.getItems().size(); i++){
-            writeList.add(MediaFileList.getItems().get(i));
+            writeMediaList.add(MediaFileList.getItems().get(i));
         }
-        data.name = writeList;
+        data.name = writeMediaList;
+
+        for (int i =0; i<UploaderList.getItems().size(); i++){
+            writeUploaderList.add(UploaderList.getItems().get(i));
+        }
+        data1.name = writeUploaderList;
+
         try {
-            ResourceManager.save(data, "1.save");
+            ResourceManager.save(data, "MediaFileList.txt");
+            ResourceManager.save(data1, "UploadFileList.txt");
+
         }
         catch (Exception e) {
             System.out.println("Couldn't save: " + e.getMessage());
@@ -85,10 +99,15 @@ public class Controller {
     @FXML
     void loadData(MouseEvent event) {
         try {
-            SaveData data = (SaveData) ResourceManager.load("1.save");
-            ObservableList<String> readList = FXCollections.observableArrayList(data.name);
+            SaveData data = (SaveData) ResourceManager.load("MediaFileList.txt");
+            ObservableList<String> readMediaList = FXCollections.observableArrayList(data.name);
 
-            MediaFileList.setItems(readList);
+            MediaFileList.setItems(readMediaList);
+
+            SaveData data1 = (SaveData) ResourceManager.load("UploadFileList.txt");
+            ObservableList<String> readUploadList = FXCollections.observableArrayList(data1.name);
+
+            UploaderList.setItems(readUploadList);
 
         }
         catch (Exception e) {
